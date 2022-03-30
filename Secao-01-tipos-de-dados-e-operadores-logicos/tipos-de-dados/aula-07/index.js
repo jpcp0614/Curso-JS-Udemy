dd314cea063328ba38940c718963478526b278b5
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 16:18:10 -0300

    Aula 09 - Boolean: Comparações

---
+++ Secao-01-tipos-de-dados-e-operadores-logicos/tipos-de-dados/aula-07/index.js
@@ -0,0 +1,7 @@
+console.log(6 > 5); // true
+console.log(6 < 5); // false
+console.log(6 >= 5); // true
+console.log(6 <= 5); // false
+console.log(6 == 5); // false
+console.log(6 != 5); // true
+console.log(6 === 5); // false
