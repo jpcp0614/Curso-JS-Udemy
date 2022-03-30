6f8ab98960a9a59b6c1ebb021ea91b18aff4a807
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 19:09:36 -0300

    Aula 02 - Variáveis: const e var

---
+++ Secao-04-estruturas-de-programacao/aula-02/index.js
@@ -0,0 +1,17 @@
+const nome = 'John Paul';
+
+console.log(nome);
+console.log(`My name is ${nome}`);
+
+const laranjas = 5;
+
+console.log(`Temos ${laranjas * 5} laranjas`);
+
+// const house = 8; // não pode
+// console.log(house);
+
+// house = 'casa';
+// console.log(house);
+
+var um = 1, dois = 2, tres = 3; // não é bom fazer assim
+console.log(`${um} + ${dois} = ${um + dois}`);
