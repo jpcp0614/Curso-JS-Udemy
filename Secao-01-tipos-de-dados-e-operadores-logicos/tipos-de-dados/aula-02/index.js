ecdd8fa6898dee6efdb3394b11e3f0ee71c7f587
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 16:15:11 -0300

    Aula - 02 Operadores

---
+++ Secao-01-tipos-de-dados-e-operadores-logicos/tipos-de-dados/aula-02/index.js
@@ -0,0 +1,6 @@
+console.log(typeof (2 + 5));
+console.log(typeof (5 - 13));
+console.log(typeof (2 * 4));
+console.log(typeof (10 / 2));
+console.log(typeof (10 % 3));
+console.log(typeof (2 ** 3));
