a6b3306fcd783c24209d20ccb850ac00ce7b67f8
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 19:05:08 -0300

    Aula 01 - Variáveis

---
+++ Secao-04-estruturas-de-programacao/aula-01/index.js
@@ -0,0 +1,17 @@
+let nome = 'John Paul';
+
+console.log(nome);
+console.log(`My name is ${nome}`);
+
+let laranjas = 5;
+
+console.log(`Temos ${laranjas * 5} laranjas`);
+
+let house = 8;
+console.log(house);
+
+house = 'casa';
+console.log(house);
+
+let um = 1, dois = 2, tres = 3; // não é bom fazer assim
+console.log(`${um} + ${dois} = ${um + dois}`);
