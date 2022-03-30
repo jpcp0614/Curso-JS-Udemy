dcd032cdb1a10d229d18254273dd91ad6c3bccb2
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 19:16:29 -0300

    Aula 03 - Convenção no nome das variáveis

---
+++ Secao-04-estruturas-de-programacao/aula-03/index.js
@@ -0,0 +1,20 @@
+// let 2dois = 2; // não pode
+
+let $name = 'John Wick';
+let $age = 30;
+let $isMan = true;
+
+let _name = 'John Wick';
+let _age = 30;
+let _isMan = true;
+
+//mais indiciado é camelCase
+let myName = 'John Wick';
+let myAge = 30;
+let isMan = true;
+
+// let @name = 'John Wick'; // não pode
+
+console.log(`Nome: ${$name}, Idade: ${$age}, Homem: ${$isMan}`);
+console.log(`Nome: ${_name}, Idade: ${_age}, Homem: ${_isMan}`);
+console.log(`Nome: ${myName}, Idade: ${myAge}, Homem: ${isMan}`);
