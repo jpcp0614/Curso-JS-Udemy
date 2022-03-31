bd6934b26c870264c4fb5c394795cf039f5f4b2a
jpcp0614 <jpcp0614@gmail.com>
2022-03-31 10:49:13 -0300

    Exercício 14

---
+++ Secao-05-exercicios-sobre-estrutura-de-programacao/Exercicio14/index.js
@@ -0,0 +1,18 @@
+// for (let i = 0; i <= 50; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`PAR: ${i}`);
+//   } else {
+//     console.log(`IMPAR: ${i}`);
+//   }
+// }
+
+let x = 0;
+
+while (x <= 50) {
+  if (x % 2 === 0) {
+    console.log(`PAR: ${x}`);
+  } else {
+    console.log(`IMPAR: ${x}`);
+  }
+  x++;
+}
