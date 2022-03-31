be5e0c8a8c8b622d208707c34143669d7418476d
jpcp0614 <jpcp0614@gmail.com>
2022-03-30 22:06:52 -0300

    Aula 09 - Estrutura de controle: if

--- Secao-04-estruturas-de-programacao/09-estrutura_if/index.js
+++ Secao-04-estruturas-de-programacao/09-estrutura_if/index.js
@@ -1,20 +1,7 @@
-let idade = 19;
+let velocidade = 75;
 
-if (idade == 19) {
-  console.log("Você é maior de idade");
-}
-if (idade > 25) {
-  console.log("Você é maior de 25")
-}
-
-let nome = 'John';
-
-if (idade > 10 && nome == 'John') {
-  console.log('Liberado 1');
-}
-
-let passaporte = true;
-
-if (nome == 'John' && idade > 30 || passaporte == true) {
-  console.log('Liberado 2');
+if (velocidade <= 80) {
+  console.log('Não foi multado');
+} else {
+  console.log('Foi multado');
 }
