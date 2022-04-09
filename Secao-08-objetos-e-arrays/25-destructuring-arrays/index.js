2c221319a341ec8e26d4379219b3b53acff20458
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 20:45:38 -0300

    25 - Destructuring com arrays

---
+++ Secao-08-objetos-e-arrays/25-destructuring-arrays/index.js
@@ -0,0 +1,17 @@
+/*
+
+* Podemos dividir varáveis com valores de um array com uma notação diferente
+
+*/
+
+let names = ['John', 'Paul', 'George', 'Ringo'];
+
+let [nameA, nameB, nameC, nameD] = names;
+
+console.log(`Beatles: ${nameA}, ${nameB}, ${nameC} e ${nameD}`);
+
+let numbers = [1, 2, 3, 4];
+
+let [num1, num2, num3, num4] = numbers;
+
+console.log(`Números: ${num1}, ${num2}, ${num3} e ${num4}`);
