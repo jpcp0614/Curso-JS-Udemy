0e6b79f305ced2d3d7f2652681d69b33e220af23
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 19:59:53 -0300

    19 - Métodos de string: padStart

---
+++ Secao-08-objetos-e-arrays/19-string-padStart/index.js
@@ -0,0 +1,6 @@
+//* inserir caracteres no inicio de uma string
+
+let milAoContrario = '1';
+
+console.log(milAoContrario);
+console.log(milAoContrario.padStart(4, '0')); //* primeiro parâmetro (quantos caracteres ao todo) e segundo (caractere a ser inserido)
