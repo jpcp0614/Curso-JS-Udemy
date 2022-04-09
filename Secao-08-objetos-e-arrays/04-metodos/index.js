c65973dcc47c1b6bc2993f0202ccbc08d5ae7dd6
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 18:30:49 -0300

    04 - Métodos

---
+++ Secao-08-objetos-e-arrays/04-metodos/index.js
@@ -0,0 +1,11 @@
+//* são propriedades que funcionam como função
+//* strings e arrays contém métodos
+
+let exemple = 'nike';
+
+console.log(exemple.toUpperCase());
+
+console.log(typeof exemple.toUpperCase());
+
+//* string.propriedade
+//* string.metodo()
