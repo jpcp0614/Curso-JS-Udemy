33d1fd329e2b9e72b5080fb7e76cd77cde3f469e
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 19:32:45 -0300

    13 - Métodos de array: indexOf e lastIndexOf

---
+++ Secao-08-objetos-e-arrays/13-indexOf-lastIndexOf/index.js
@@ -0,0 +1,4 @@
+let numbers = [1, 2, 3, 4, 5, 6, 3];
+
+console.log(numbers.indexOf(3)); //* retorna o índice do elemento
+console.log(numbers.lastIndexOf(3)); //* retorna o índice do elemento, mas começa a busca do final do array
