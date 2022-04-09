057631dd0c08d9192e729cad553b0cbe821fb6ee
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 19:36:59 -0300

    14 - Métodos de array: slice

---
+++ Secao-08-objetos-e-arrays/14-slice/index.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5, 6, 3];
+
+//* o slice retorna um array a partir de outro array
+//* o array retornado é determinado pelos parâmetros que são
+//* o primeiro índice e o fim do novo array
+
+console.log(numbers.slice(0, 5)); //* retorna um array com os elementos do índice 0 até o índice 5-1 (4)
+console.log(numbers.slice(4)); //* retorna um array com os elementos do índice 4 até o final do array
