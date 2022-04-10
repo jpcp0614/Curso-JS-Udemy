5f78c98db4578efd0518d75a5fc87168511286c9
jpcp0614 <jpcp0614@gmail.com>
2022-04-10 19:32:34 -0300

    02 - Strict Mode

---
+++ Secao-12-debugs-e-erros-js/02-strict-mode/index.js
@@ -0,0 +1,21 @@
+// "use strict";
+/*
+
+TODO Strict mode
+
+* Deixa o JS mais rigoroso na hora de se programar
+
+* Deve ser declarado no topo de arquivos ou dentro de funções
+
+* Colocar o strict ajuda você a evitar erros de programação, evitando assim, bugs
+
+*/
+
+// opa = 'teste';
+
+// delete Object.prototype;
+
+(function testando() {
+  "use strict";
+  opa = 'teste';
+})();
