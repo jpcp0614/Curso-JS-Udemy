35b98ebc987199ba7f8638d4c254132c4fcbde3c
jpcp0614 <jpcp0614@gmail.com>
2022-04-09 21:16:10 -0300

    Exercício 28

---
+++ Secao-09-exercicios-sobre-arrays-e-objetos/Exercicio28/index.js
@@ -0,0 +1,8 @@
+const bus = {
+  wheel: 8,
+  seats: 40,
+  doors: 2,
+}
+
+console.log(`As propriedades do objeto bus são: ${Object.keys(bus)}`);
+console.log(`O valor das propriedades do objeto bus são: ${Object.values(bus)}`);
